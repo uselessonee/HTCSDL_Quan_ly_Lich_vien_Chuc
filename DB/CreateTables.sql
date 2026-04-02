602073287bc685a1f42ee4824703f1fd583d534d
Tevel <119736325+Tadataka-Rei@users.noreply.github.com>
2026-04-03 01:37:45 +0700

    feat(CreateTables): Add TrinhDoHocVan table with test  fields

--- DB/CreateTables.sql
+++ DB/CreateTables.sql
@@ -35,3 +35,16 @@ CREATE TABLE VienChuc (
     SoHieuVienChuc NVARCHAR(255) NOT NULL,
     AnhDaiDien LONGBLOB, -- Cần thảo luận thêm về kiểu dữ liệu lưu ảnh, có thể là VARBINARY(MAX) nếu chết não
 );
+
+
+---- Tạo bảng trình Độ Học Vấn ---------
+CREATE TABLE TrinhDoHocVan (
+    MaTrinhDo INT PRIMARY KEY, -- Mã trình độ học vấn, cần thảo luận thêm với nhóm để xác định kiểu dữ liệu
+    TrinhDoGiaoDucPhoThong NVARCHAR(255) NOT NULL, --- Cần thảo luận thêm về cách lưu trình độ giáo dục phổ thông, có thể là ENUM hoặc bảng phụ nếu có nhiều loại
+    TrinhDoChuyenMonCaoNhat NVARCHAR(255) NOT NULL, -- Cần thảo luận thêm về cách lưu trình độ chuyên môn cao nhất, có thể là ENUM hoặc bảng phụ nếu có nhiều loại
+    LyLuanChinhTri NVARCHAR(255) NOT NULL, -- Cần thảo luận thêm về cách lưu lý luận chính trị, có thể là ENUM hoặc bảng phụ nếu có nhiều loại
+    QuanLyNhaNuoc NVARCHAR(255) NOT NULL, -- Cần thảo luận thêm về cách lưu quản lý nhà nước, có thể là ENUM hoặc bảng phụ nếu có nhiều loại
+    TrinhDoNghiepVuChuyenNganh NVARCHAR(255) NOT NULL, -- Cần thảo luận thêm về cách lưu trình độ nghiệp vụ chuyên ngành, có thể là ENUM hoặc bảng phụ nếu có nhiều loại
+    NgoaiNgu NVARCHAR(255) NOT NULL, -- Cần thảo luận thêm về cách lưu ngoại ngữ, có thể là ENUM hoặc bảng phụ nếu có nhiều loại
+    -- TinHoc Enum() Chịu đuối quá rồi, mai hỏi hai đứa kia
+);
